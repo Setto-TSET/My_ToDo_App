3a54be6e0de5919f344e0319413015a05f033181
SETT0-TSET <setthawat.phomkaew@gmail.com>
2026-03-01 05:59:32 +0700

    Fix PORT variable for Railway 2

--- my-todo-backend/server.js
+++ my-todo-backend/server.js
@@ -229,5 +229,5 @@ app.delete('/api/tasks/:id', authenticateToken, async (req, res) => {
 const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(` Server running on port ${PORT}`);
 });
